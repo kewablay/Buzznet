371a3c49ea9c85a15819631967bdb52c316ae365
kewablay <kojokewajunior123@gmail.com>
2024-09-15 01:55:21 +0000

    added post detail content

--- src/app/pages/post-detail/post-detail.component.ts
+++ src/app/pages/post-detail/post-detail.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { PostCardComponent } from "../../components/post-card/post-card.component";
 import { CommentListComponent } from "../../components/comment-list/comment-list.component";
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: 'app-post-detail',
   standalone: true,
-  imports: [HeaderComponent, PostCardComponent, CommentListComponent],
+  imports: [HeaderComponent, PostCardComponent, CommentListComponent, CardComponent],
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.sass'
 })
