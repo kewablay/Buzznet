06c0f7a60e503c29ebe0b1e338252106b640a49b
kewablay <kojokewajunior123@gmail.com>
2024-09-15 13:50:41 +0000

    updated post card

--- src/app/components/post-card/post-card.component.ts
+++ src/app/components/post-card/post-card.component.ts
@@ -30,6 +30,16 @@ export class PostCardComponent {
 
   deletePost(id: number, event: Event) {
     event.stopPropagation();
-    this.api.deletePost(id);
+    this.api.deletePost(id).subscribe({
+      next: (data) => {
+        console.log('Post deleted successfully', data);
+        this.api
+          .getPosts()
+          .subscribe((data) => console.log('data after delete: ', data));
+      },
+      error: (error) => {
+        console.error('Error deleting post:', error);
+      },
+    });
   }
 }
