59be8046d31048fe17b6d22cc92fa2c8ac4d799e
kewablay <kojokewajunior123@gmail.com>
2024-09-15 09:33:41 +0000

    updated comment list to display comments

--- src/app/components/comment-list/comment-list.component.ts
+++ src/app/components/comment-list/comment-list.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { CardComponent } from "../card/card.component";
+import { Component, Input } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+import { ApiService } from '../../services/api.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { Comment } from '../../models/app.model';
 
 @Component({
   selector: 'app-comment-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, AsyncPipe],
   templateUrl: './comment-list.component.html',
-  styleUrl: './comment-list.component.sass'
+  styleUrl: './comment-list.component.sass',
 })
 export class CommentListComponent {
+  @Input() postId!: number;
+  comments$: Observable<Comment[]>;
+  constructor(private api: ApiService) {
+    this.comments$ = this.api.getPostComments(this.postId);
+  }
 
+  ngOnChanges() {
+    this.comments$ = this.api.getPostComments(this.postId);
+  }
 }
