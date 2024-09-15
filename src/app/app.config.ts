0197ed5480dfdac260b41a0261ff7a977504284a
kewablay <kojokewajunior123@gmail.com>
2024-09-15 13:50:20 +0000

    added browser animations module to providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideStore()],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideStore(),
+    BrowserModule,
+    importProvidersFrom(BrowserAnimationsModule),
+  ],
 };
