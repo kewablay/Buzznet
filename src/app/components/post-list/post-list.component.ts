6f44c7b028e7aa0c5ff3d77e68ed8597adeb52af
kewablay <kojokewajunior123@gmail.com>
2024-09-15 01:54:34 +0000

    added post list content

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PostCardComponent } from "../post-card/post-card.component";
+import { CardComponent } from "../card/card.component";
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [],
+  imports: [PostCardComponent, CardComponent],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.sass'
 })
