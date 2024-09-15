470bffb83d8e1c8c48cedff407300bb9cc187829
kewablay <kojokewajunior123@gmail.com>
2024-09-15 03:34:53 +0000

    updated post list to show post from api

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { PostCardComponent } from "../post-card/post-card.component";
-import { CardComponent } from "../card/card.component";
+import { PostCardComponent } from '../post-card/post-card.component';
+import { CardComponent } from '../card/card.component';
+import { Post } from '../../models/posts.model';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [PostCardComponent, CardComponent],
+  imports: [PostCardComponent, CardComponent, AsyncPipe, RouterLink],
   templateUrl: './post-list.component.html',
-  styleUrl: './post-list.component.sass'
+  styleUrl: './post-list.component.sass',
 })
 export class PostListComponent {
+  posts$: Observable<Post[]>;
 
+  constructor(private api: ApiService) {
+    this.posts$ = this.api.getPosts();
+  }
+
+  ngOnInit() {
+    this.posts$.subscribe((data) => console.log(data));
+  }
 }
